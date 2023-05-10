8f562b17fb70b8fc54c86ceecbfcbf10098d0e7b
verisky <verisky.jaya@astronauts.id>
2023-05-10 14:51:28 +0700

    init

--- vars/conditionalRetry.groovy
+++ vars/conditionalRetry.groovy
@@ -1,11 +1,11 @@
-def call(Integer maxRetries, String condition, body) {
+def call(Integer maxRetries, Predicate<String> predicate, body) {
     def config = [:]
     def retries = 0
     body.resolveStrategy = Closure.OWNER_FIRST
     body.delegate = config
 
     retry (maxRetries) {
-        if (retries == 0 || readFile("output.txt").contains(condition)) {
+        if (retries == 0 || predicate.test("")) {
             body()
         } else {
             error("Build doesn't fulfill conditional to retry.")
