dd98afdacb21391ad45a24057b9468eec7d38c21
verisky <verisky.jaya@astronauts.id>
2023-08-14 10:43:29 +0700

    set match as array

--- vars/conditionalRetry.groovy
+++ vars/conditionalRetry.groovy
@@ -1,11 +1,11 @@
-def call(Integer maxRetries, String filename, String match, body) {
+def call(Integer maxRetries, String filename, String[] matches, body) {
     def config = [:]
     def retries = 0
     body.resolveStrategy = Closure.OWNER_FIRST
     body.delegate = config
 
     retry (maxRetries) {
-        if (retries == 0 || readFile(filename).contains(match)) {
+        if (retries == 0 || matches.any{el -> readFile(filename).contains(el)}) {
             body()
         } else {
             error("Build doesn't fulfill conditional to retry.")
