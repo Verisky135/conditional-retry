3e604cac093ae76bd099c64c043f2deda77416d8
verisky <verisky.jaya@astronauts.id>
2023-08-16 14:47:35 +0700

    chores update error message

--- vars/conditionalRetry.groovy
+++ vars/conditionalRetry.groovy
@@ -1,19 +1,19 @@
-def call(Integer maxRetries = 5, String filename, String[] match, Integer retrySleep = 10, Boolean expBackoff = true, body) {
+def call(Integer retries = 5, String filename, String[] match, Integer retrySleep = 10, Boolean expBackoff = true, body) {
     def config = [:]
-    def retries = 0
+    def retryIndex = 0
     body.resolveStrategy = Closure.OWNER_FIRST
     body.delegate = config
 
-    retry (maxRetries) {
-        if (retries == 0 || match.any{el -> readFile(filename).contains(el)}) {
-            if (retries > 0 && retrySleep > 0) {
+    retry (retries) {
+        if (retryIndex == 0 || match.any{el -> readFile(filename).contains(el)}) {
+            if (retryIndex > 0 && retrySleep > 0) {
                 if (expBackoff) {
-                    sleep(retrySleep * retries)
+                    sleep(retrySleep * retryIndex)
                 } else {
                     sleep(retrySleep)
                 }
             }
-            retries += 1
+            retryIndex += 1
             body()
         } else {
             error("Skipping retry : build doesn't fulfill condition to retry.")
