f85344fb64ed5afddb0f001c2fece15469116daa
verisky <verisky.jaya@astronauts.id>
2023-08-16 14:49:19 +0700

    chores update error message

--- vars/conditionalRetry.groovy
+++ vars/conditionalRetry.groovy
@@ -1,11 +1,11 @@
-def call(Integer retries = 5, String filename, String[] match, Integer retrySleep = 10, Boolean expBackoff = true, body) {
+def call(Integer retries = 5, String filename="output.txt", String[] errorMsg, Integer retrySleep = 10, Boolean expBackoff = true, body) {
     def config = [:]
     def retryIndex = 0
     body.resolveStrategy = Closure.OWNER_FIRST
     body.delegate = config
 
     retry (retries) {
-        if (retryIndex == 0 || match.any{el -> readFile(filename).contains(el)}) {
+        if (retryIndex == 0 || errorMsg.any{el -> readFile(filename).contains(el)}) {
             if (retryIndex > 0 && retrySleep > 0) {
                 if (expBackoff) {
                     sleep(retrySleep * retryIndex)
